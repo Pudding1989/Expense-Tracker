ee8eeb7f2b28eb9c28bff27907b5610389fb9f51
Pudding1989 <budding1989@outlook.com>
2021-08-28 19:21:21 +0800

    feat: 增加 index page、 main layout

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,12 +2,37 @@ const express = require('express')
 const router = express.Router()
 
 Record = require('../../models/record')
+iconSet = require('../../models/icon')
 
 router.get('/', (req, res) => {
   Record
     .find()
     .lean()
-    .then(res.send('<h1>Hi~ Express Service connected Success<h1>'))
+    .then(recordData => {
+     //計算總金額
+      let totalAmount = 0
+
+      recordData.forEach(data => {
+        //順便計算總金額
+        totalAmount += data.amount
+        //增加icon類別，對應font awesome
+        switch (data.category) {
+          case '家居物業': data.icon = iconSet.house
+            break
+          case '交通出行': data.icon = iconSet.van
+            break
+          case '休閒娛樂': data.icon = iconSet.grin
+            break
+          case '餐飲食品': data.icon = iconSet.tableware
+            break
+          case '其他': data.icon = iconSet.pen
+          default: data.icon = iconSet.pen
+            break
+        }
+
+      })
+      res.render('index', { recordData, totalAmount })
+    })
     .catch(error => console.error(error))
 })
 
